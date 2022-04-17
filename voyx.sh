5127a6c946c49ee4d903a23a724e2060017ef43e
Jürgen Hock <juergen.hock@jurihock.de>
2022-04-17 13:16:54 +0200

    Update voyx.sh

--- voyx.sh
+++ voyx.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
+# ###########################################################################
+#
+# The qt@5 is keg-only, which means it was not symlinked into /opt/homebrew,
+# because this is an alternate version of another formula.
+#
+# If you need to have qt@5 first in your PATH, run:
+#   echo 'export PATH="/opt/homebrew/opt/qt@5/bin:$PATH"' >> ~/.zshrc
+#
+# For compilers to find qt@5 you may need to set:
+#   export LDFLAGS="-L/opt/homebrew/opt/qt@5/lib"
+#   export CPPFLAGS="-I/opt/homebrew/opt/qt@5/include"
+#
 PATH="/opt/homebrew/opt/qt@5/bin:$PATH"
+#
+# ###########################################################################
 
 mkdir -p build-release
 
