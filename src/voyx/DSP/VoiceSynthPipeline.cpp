8113ba9b15a147645db71c6f9cd4ca7e5940c8d7
Jürgen Hock <juergen.hock@jurihock.de>
2022-05-11 21:22:40 +0200

    Update VoiceSynthPipeline.cpp

--- src/voyx/DSP/VoiceSynthPipeline.cpp
+++ src/voyx/DSP/VoiceSynthPipeline.cpp
@@ -42,15 +42,50 @@ void VoiceSynthPipeline::operator()(const size_t index, const std::vector<voyx_t
 
   vocoder.encode(dfts);
 
-  const voyx_t factor = 2;
+  const std::vector<voyx_t> factors = { 0.5, 1.25, 1.5, 2 };
+
+  std::vector<std::complex<voyx_t>> buffer(factors.size() * dfts.front().size());
+  std::vector<std::span<std::complex<voyx_t>>> buffers(factors.size());
+  std::vector<size_t> mask(dfts.front().size());
+
+  for (size_t i = 0; i < factors.size(); ++i)
+  {
+    buffers[i] = std::span<std::complex<voyx_t>>(
+      buffer.data() + i * dfts.front().size(),
+      dfts.front().size());
+  }
 
   for (auto dft : dfts)
   {
-    $$::interp(dft.size(), dft.data(), dft.data(), factor);
+    for (size_t i = 0; i < factors.size(); ++i)
+    {
+      // FIXME $$::interp(dft, buffers[i], factors[i]);
+      $$::interp(dft.size(), dft.data(), buffers[i].data(), factors[i]);
+    }
+
+    for (size_t i = 0; i < dfts.front().size(); ++i)
+    {
+      size_t index = 0;
+      voyx_t value = buffers[0][i].real();
+
+      for (size_t j = 1; j < factors.size(); ++j)
+      {
+        if (buffers[j][i].real() > value)
+        {
+          index = j;
+          value = buffers[j][i].real();
+        }
+      }
+
+      mask[i] = index;
+    }
 
     for (size_t i = 0; i < dft.size(); ++i)
     {
-      dft[i].imag(dft[i].imag() * factor);
+      const size_t j = mask[i];
+
+      dft[i].real(buffers[j][i].real());
+      dft[i].imag(buffers[j][i].imag() * factors[j]);
     }
   }
 
