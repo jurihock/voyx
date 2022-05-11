31d0edc188554dde3468c58881e4b9859727bd27
Jürgen Hock <juergen.hock@jurihock.de>
2022-05-11 19:37:19 +0200

    Update cpm.cmake

--- lib/cpm.cmake
+++ lib/cpm.cmake
@@ -1,7 +1,7 @@
 # https://github.com/cpm-cmake/CPM.cmake
 
-set(CPMDST "${CMAKE_BINARY_DIR}/CPM.cmake")
 set(CPMSRC "https://github.com/cpm-cmake/CPM.cmake/releases/download/v0.35.0/CPM.cmake")
+set(CPMDST "${CMAKE_BINARY_DIR}/CPM.cmake")
 
 if(NOT EXISTS "${CPMDST}")
   file(DOWNLOAD "${CPMSRC}" "${CPMDST}")
