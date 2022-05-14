ab14f3804fc14b95a5507678d475d0bdf9c10de5
Jürgen Hock <juergen.hock@jurihock.de>
2022-05-14 21:26:45 +0200

    Include metal

---
+++ lib/metal.cmake
@@ -0,0 +1,34 @@
+# https://developer.apple.com/metal/cpp
+
+CPMAddPackage(
+  NAME metal
+  VERSION 2021.11.09
+  URL https://developer.apple.com/metal/cpp/files/metal-cpp_macOS12_iOS15.zip
+  DOWNLOAD_ONLY YES)
+
+if(metal_ADDED)
+
+  add_library(metal INTERFACE)
+
+  if(CMAKE_SYSTEM_NAME STREQUAL "Darwin")
+
+    target_include_directories(metal
+      INTERFACE "${metal_SOURCE_DIR}")
+
+    target_link_libraries(metal
+      INTERFACE "-framework Foundation"
+                "-framework Metal"
+                "-framework MetalKit")
+
+    target_compile_definitions(metal
+      INTERFACE -DCA_PRIVATE_IMPLEMENTATION)
+
+    target_compile_definitions(metal
+      INTERFACE -DMTL_PRIVATE_IMPLEMENTATION)
+
+    target_compile_definitions(metal
+      INTERFACE -DNS_PRIVATE_IMPLEMENTATION)
+
+  endif()
+
+endif()
