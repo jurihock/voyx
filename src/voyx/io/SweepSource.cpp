eac9869fb3539dbc88e6a50c61d2fb9930d3857a
Jürgen Hock <juergen.hock@jurihock.de>
2022-05-25 19:43:18 +0200

    Update sweep

--- src/voyx/io/SweepSource.cpp
+++ src/voyx/io/SweepSource.cpp
@@ -2,29 +2,32 @@
 
 #include <voyx/Source.h>
 
-SweepSource::SweepSource(std::pair<voyx_t, voyx_t> frequency, voyx_t duration, voyx_t samplerate, size_t framesize, size_t buffersize) :
-  SweepSource(1, frequency, duration, samplerate, framesize, buffersize)
+SweepSource::SweepSource(std::pair<voyx_t, voyx_t> frequencies, voyx_t duration, voyx_t samplerate, size_t framesize, size_t buffersize) :
+  SweepSource(1, frequencies, duration, samplerate, framesize, buffersize)
 {
 }
 
-SweepSource::SweepSource(voyx_t amplitude, std::pair<voyx_t, voyx_t> frequency, voyx_t duration, voyx_t samplerate, size_t framesize, size_t buffersize) :
+SweepSource::SweepSource(voyx_t amplitude, std::pair<voyx_t, voyx_t> frequencies, voyx_t duration, voyx_t samplerate, size_t framesize, size_t buffersize) :
   Source(samplerate, framesize, buffersize),
   amplitude(amplitude),
-  frequency(frequency),
+  frequencies(frequencies),
   duration(duration),
   lfo(1 / duration, samplerate),
-  sweep(frequency.first, samplerate),
+  sine(frequencies.first, samplerate),
   frame(framesize)
 {
 }
 
 bool SweepSource::read(const size_t index, std::function<void(const voyx::vector<voyx_t> frame)> callback)
 {
+  const voyx_t slope = (frequencies.second - frequencies.first) / 2;
+  const voyx_t intercept = (frequencies.second + frequencies.first) / 2;
+
   for (size_t i = 0; i < frame.size(); ++i)
   {
-    const voyx_t f = (lfo() * 0.5 + 0.5) * (frequency.second - frequency.first) + (frequency.first);
+    const voyx_t frequency = lfo() * slope + intercept;
 
-    frame[i] = amplitude * sweep(f);
+    frame[i] = amplitude * sine(frequency);
   }
 
   callback(frame);
--- src/voyx/io/SweepSource.h
+++ src/voyx/io/SweepSource.h
@@ -9,19 +9,19 @@ class SweepSource : public Source<voyx_t>
 
 public:
 
-  SweepSource(std::pair<voyx_t, voyx_t> frequency, voyx_t duration, voyx_t samplerate, size_t framesize, size_t buffersize);
-  SweepSource(voyx_t amplitude, std::pair<voyx_t, voyx_t> frequency, voyx_t duration, voyx_t samplerate, size_t framesize, size_t buffersize);
+  SweepSource(std::pair<voyx_t, voyx_t> frequencies, voyx_t duration, voyx_t samplerate, size_t framesize, size_t buffersize);
+  SweepSource(voyx_t amplitude, std::pair<voyx_t, voyx_t> frequencies, voyx_t duration, voyx_t samplerate, size_t framesize, size_t buffersize);
 
   bool read(const size_t index, std::function<void(const voyx::vector<voyx_t> frame)> callback) override;
 
 private:
 
   const voyx_t amplitude;
-  const std::pair<voyx_t, voyx_t> frequency;
+  const std::pair<voyx_t, voyx_t> frequencies;
   const voyx_t duration;
 
   Oscillator<voyx_t> lfo;
-  Oscillator<voyx_t> sweep;
+  Oscillator<voyx_t> sine;
 
   std::vector<voyx_t> frame;
 
