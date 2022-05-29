899ab9ad2338ca0e734ec03f50ae115969203233
Jürgen Hock <juergen.hock@jurihock.de>
2022-05-29 22:48:02 +0200

    Add abs overload

--- src/voyx/etc/Convert.Common.h
+++ src/voyx/etc/Convert.Common.h
@@ -4,19 +4,31 @@
 
 namespace $$
 {
+  struct abs
+  {
+    template<typename T>
+    T operator()(const T value) const { return value; };
+
+    template<typename T>
+    T operator()(const std::complex<T>& value) const { return std::abs(value); };
+  };
+
   struct real
   {
     template<typename T>
     T operator()(const T value) const { return value; };
 
     template<typename T>
-    T operator()(const std::complex<T>& value) const { return value.real(); };
+    T operator()(const std::complex<T>& value) const { return std::real(value); };
   };
 
   struct imag
   {
     template<typename T>
-    T operator()(const std::complex<T>& value) const { return value.imag(); };
+    T operator()(const T value) const { return T(0); };
+
+    template<typename T>
+    T operator()(const std::complex<T>& value) const { return std::imag(value); };
   };
 }
 
